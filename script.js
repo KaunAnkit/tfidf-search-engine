577eec0856bc6c4673b2243ffa50f40849b95c85
Ankit Jha <kaunankitjha@gmail.com>
2026-06-11 14:56:42 +0530

    Fixed URL

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5500/search";
+const API_URL = "https://search-engine-node-backend.onrender.com/search";
 
 const searchBtn = document.getElementById("searchBtn");
 const queryInput = document.getElementById("query");
